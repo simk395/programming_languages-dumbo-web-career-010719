c940207ee353487cafc5b42c56d7dd336c2e8c4d
Simon Kong <simon.kong95@gmail.com>
2018-12-19 05:07:53 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,75 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  i = {}
+  languages.each do |object, lang|
+    i.merge(lang)
+    lang.each do |name, type|
+      binding.pry
+    end
+  end
 end
+
+=begin
+```ruby
+
+i = 
+{
+:ruby => {
+  :type => "interpreted"
+},
+:javascript => {
+  :type => "interpreted"
+},
+:python => {
+  :type => "interpreted"
+},
+:java => {
+  :type => "compiled"
+}
+:clojure => {
+  :type => "compiled"
+},
+:erlang => {
+  :type => "compiled"
+},
+:scala => {
+  :type => "compiled"
+},
+:javascript => {
+  :type => "interpreted"
+}
+}
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+=end
