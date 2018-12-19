87ed57cfb3bc94469e25cad82840344fd8df4055
Simon Kong <simon.kong95@gmail.com>
2018-12-19 05:19:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,7 +35,7 @@ languages = {
 def reformat_languages(languages)
   i = {}
   languages.each do |object, lang|
-    i.merge{lang}
+    i.merge!(lang)
     lang.each do |name, type|
       binding.pry
     end
